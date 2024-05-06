904c74169b9bfc542bdc583c4a64d796538228bb
sunilpodalakuru <sunilpodalakuru958@gmail.com>
2024-05-06 10:29:09 +0530

    Add files via upload

--- week2/Atsw.py
+++ week2/Atsw.py
@@ -1,9 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 t=np.arange(0,1,0.01)
-f=5
-x=np.sin(2*np.pi*f*t)
-plt.plot(t,x)
+f1=5
+x=np.sin(2*np.pi*f1*t)
+f2=3
+y=np.sin(2*np.pi*f2*t)
+k=x+y
+plt.plot(t,k)
 plt.xlabel('time')
 plt.ylabel('amplitude')
 plt.title('sine wave')
