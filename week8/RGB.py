fc656face63f831a049c57fcd2d1265cc64f9621
sunilpodalakuru <sunilpodalakuru958@gmail.com>
2024-05-06 10:43:30 +0530

    Add files via upload

--- week8/RGB.py
+++ week8/RGB.py
@@ -1,1 +1,15 @@
+input file='/home/rguktrkvalley/13.jpg';
+imagedata=imread(inputfile);
+redchannel=imagedata(:,:,1);
+greenchannel=imagedata(:,:,2);
+bluechannel=imagedata(:,:,3);
 
+redoutputfile='red_channel.csv';
+greenoutputfile='green_channel.csv';
+blueoutputfile='blue_channel.csv';
+
+cswrite(redoutputfile,redchannel);
+cswrite(greenoutputfile,greenchannel);
+cswrite(blueoutputfile,bluechannel);
+
+disp('red,green and blue channels saved to separate csv files successfully!');
